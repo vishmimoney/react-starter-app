90573f2c119de7f9077d5edad7c2247c645819a8
vishmimoney <vishmimoney@gmail.com>
2019-02-17 20:49:14 +0100

    create app for start a react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MyComponent from './components/myComponent.js'
+
+class App extends Component {
+  render() {
+    return (
+      <MyComponent></MyComponent>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/myComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                <div>My Component</div>
+                <p>You can start write your own component here.</p>
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
